467b5a25f1f7fa437a7091b811e2f65e1b083e81
AlejandroFlorez1 <alejofl2003@gmail.com>
2024-04-10 09:59:26 -0500

    v1.01
    Se agrega el login y el home, aun no funciona

--- DjangoGreenInnProyect/urls.py
+++ DjangoGreenInnProyect/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from reservas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('productos/', views.Productos, name='productos'),
+    path('home/', views.home, name='home'),
+    path('loguot/', exit, name='exit'),
 ]
+ 
--- reservas/views.py
+++ reservas/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
+
+def helloword(request):
+    return render(request, 'Login/Login.html', {'form': UserCreationForm})
+
+@login_required
+def Productos(request):
+    return render(request, 'Core/main.html', {'form': UserCreationForm})
+
+def home(request):
+    return render(request, 'Core/home.html', {'form': UserCreationForm})
+
+def exit(request):
+    logout(request)
+    return redirect('home')
