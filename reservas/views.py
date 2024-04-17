b1ee71a34091fed40c0dc0d30ccf9bb57be11e56
AlejandroFlorez1 <alejofl2003@gmail.com>
2024-04-17 15:51:12 -0500

    17/04 15:50 Uptade, se divide el principal en varias partes."

--- reservas/views.py
+++ reservas/views.py
@@ -13,8 +13,21 @@ def login(request):
     return render(request, 'registration/login.html', {'form': UserCreationForm})
 
 def home(request):
+<<<<<<< Updated upstream
     return render(request, 'Core/prueba.html', {'form': UserCreationForm})
+=======
+    return render(request, 'Core/principalHome.html', {'form': UserCreationForm})
+>>>>>>> Stashed changes
 
 def exit(request):
     logout(request)
     return redirect('home')
+
+def Acercade(request):
+    return render (request, 'Core/acercaDeHome.html')
+
+def Cabañas(request):
+    return render (request, 'Core/cabañasHome.html')
+
+def Restaurante(request):
+    return render (request, 'Core/restauranteHome.html')
